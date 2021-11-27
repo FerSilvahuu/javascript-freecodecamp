2ffd1912f884cff1f9f0e9de361fa6a095e71601
Feerr S <feerrsilvav@gmail.com>
2021-11-26 18:15:50 -0600

    Agregamos lecciones pendientes

--- lessons/for.js
+++ lessons/for.js
@@ -1,3 +1,32 @@
 // For se ejectuta "por" un número de veces
+const myArr = [2,3,4,5,6];
 
-    
+let total = 0;
+
+for (let i =0; i < myArr.length; i ++) {
+    total = total + myArr[i];
+}
+// console.log(total)
+
+// Anidar blucles for
+
+const arr = [
+    [1,2],[3,4],[5,6]
+];
+
+for (let i =0; i < arr.length; i++) {
+    for(let b=0; b < arr[i].length; b++) {
+        //console.log(arr[i][b]);
+    }
+}
+
+const abc = (arr) =>{
+    let product =1;
+    for(let i=0; i < arr.length; i ++) {
+        for(let b =0; b < arr[i].length; b++) {
+            product = product * arr[i][b]
+        }
+    }
+    return product;
+}
+// abc([[1, 2], [3, 4], [5, 6, 7]]);
---
+++ lessons/recursion.js
@@ -0,0 +1,47 @@
+// Recursion 
+
+// Cuanto una función se llama a si misma hasta que deja de hacerlo
+
+for (let i = 5; i  > 0; i --) {
+    // console.log(i);
+}
+
+let cuentaAtras = numero => {
+    // base case 
+    if (numero === 0) {
+        return;
+    }
+    // console.log(numero);
+    return cuentaAtras(numero - 1);
+};
+
+// console.log(cuentaAtras(5));
+// La entrada actual es 5
+// Es 5 igual a 0 ?
+// No, Ok entonces imprime 5 en la consola.
+// Se llama asi misma de nuevo con el numero - 1 O 5 - 1;
+// La entrada principal es 4
+// Es 4 igual a 0 ?
+// No, Ok entonces imprime 4 en la consola.
+// Repite hasta que la entrada sea 0, y asi la función deja de llamarse a si misma.
+
+let parImpar = numero => {
+    if (numero === 0) {
+        return 'par';
+    } else if ( numero === 1) {
+        return 'Impar'
+    } else  {
+        return parImpar(numero -2)
+    }
+};
+
+// Leer una palabra alreves
+
+let reto = 'FreeCodeCamp';
+
+const atras = reto => {
+    if (reto = '') {
+        return console.log('cadena vacia')
+    } else if 
+}
+
